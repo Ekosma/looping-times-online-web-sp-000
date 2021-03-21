8e1dd39a721dcd1cae7e4b7ce61c471e8ad309e6
Evelyn Kosma <ezranaja@gmail.com>
2021-03-21 19:50:32 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,9 +1,12 @@
+<<<<<<< HEAD
 def using_times
 	7.times do 
   puts "Wingardium Leviosa"
   end
+=======
+7.times do 
+  puts "Wingardium Leviosa"
+>>>>>>> a6923ce08416ae50aa27954ac5f4b5e8a3f451a7
 end
-
-
 
 
